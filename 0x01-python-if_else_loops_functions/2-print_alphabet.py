0f379ecd94cb455f6680caefa7c4fae67bf4a383
Juma Omondi John  Paul <105412648+Omondi-Jp@users.noreply.github.com>
2022-11-30 03:38:28 +0300

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,7 +2,5 @@
 # 2-print_alphabet.py
 
 """Print the alphabet in lowercase, not followed by a new line."""
-for alpha_letters in range(ord('a'), ord('z')+1):
-    if alpha_letters == 'e' or alpha_letters == 'q':
-       continue
-    print("{:c}".format(alpha_letters), end="")
+for letter in range(97, 123):
+    print("{:c}".format(chr(letter)), end="")
