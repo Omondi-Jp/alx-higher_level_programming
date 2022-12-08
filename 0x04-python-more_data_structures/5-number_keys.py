0439437da7ad1eb63e4720c903e0c27d58ee4a5e
Juma Omondi John  Paul <105412648+Omondi-Jp@users.noreply.github.com>
2022-12-07 18:24:50 -0800

    Create 5-number_keys.py

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 5-number_keys.py
+
+
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary."""
+    return (len(a_dictionary))
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pr
