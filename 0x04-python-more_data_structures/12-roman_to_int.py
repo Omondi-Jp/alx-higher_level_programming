564f1019d91a48b9cee6782d4f2713df390badbf
Juma Omondi John  Paul <105412648+Omondi-Jp@users.noreply.github.com>
2022-12-07 18:20:07 -0800

    Create 12-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 12-roman_to_int.py
+
+
+def roman_to_int(roman_string):
+    """Converts a roman numeral to an integer."""
+    if (not isinstance(roman_string, str) or
+            roman_string is None):
+        return (0)
+
+    roman_dict = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+    }
+    num = 0
